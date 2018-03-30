9ccf630a4a3cd22597d1684749787649b958da80
Zukra <zluka.888@gmail.com>
2018-03-30 15:09:54 +0300

    add finding phone

--- TelegramHistory.php
+++ TelegramHistory.php
@@ -2,12 +2,18 @@
 
 namespace zkr;
 
+use libphonenumber\PhoneNumberUtil;
+
 class TelegramHistory {
 
     public $inFile = 'data/in.txt';
     public $tmpFile = 'data/tmp.txt';
     public $emailFile = 'data/email.data';
+    public $phoneFile = 'data/phone.data';
     public $arrEmails = [];
+    public $arrPhones = [];
+    public $countryCodes = ['RU', 'KZ'];
+//    public $countryCodes = ['RU', 'BY', 'KZ', 'UA'];
 
     public function getHistory(string $inFile) {
         $content = file_get_contents($inFile);
@@ -29,9 +35,10 @@ class TelegramHistory {
 //        $this->arrEmails[$item[0]]['date'][] = $item[1];
         if (empty($this->arrEmails[$item[0]]['msg']) || !in_array($item[2], $this->arrEmails[$item[0]]['msg'])) {
             $this->arrEmails[$item[0]]['msg'][] = $item[2];
-        }
-        foreach ($emailMatches[0] as $email) {
-            $this->arrEmails[$item[0]]['email'][$email] = $email;
+
+            foreach ($emailMatches[0] as $email) {
+                $this->arrEmails[$item[0]]['email'][$email] = $email;
+            }
         }
     }
 
@@ -41,6 +48,38 @@ class TelegramHistory {
         foreach ($this->arrEmails as $author => $item) {
             $sEmails .= $author . ' ; ' . implode(' ', $item['email']) . "\n";
         }
+
         return file_put_contents($this->emailFile, $sEmails);
+    }
+
+    public function getPhones($item) {
+        //        $this->arrPhones[$item[0]]['date'][] = $item[1];
+        $phoneNumberUtil = PhoneNumberUtil::getInstance();
+        $text = $item[2];
+        foreach ($this->countryCodes as $code) {
+            $phoneNumberMatcher = $phoneNumberUtil->findNumbers($text, $code);
+            foreach ($phoneNumberMatcher as $phoneNumberMatch) {
+
+                if (empty($this->arrPhones[$item[0]]['msg']) || !in_array($item[2], $this->arrPhones[$item[0]]['msg'])) {
+                    $this->arrPhones[$item[0]]['msg'][] = $item[2];
+                }
+
+                $number = $phoneNumberMatch->number();
+                $key = $number->getCountryCode() . '_' . $number->getNationalNumber();
+                $this->arrPhones[$item[0]]['phone'][$key] = $number->getCountryCode() . ' ' . $number->getNationalNumber();
+            }
+        }
+
+        return $this->arrPhones;
+    }
+
+    public function storePhones() {
+        ksort($this->arrPhones);
+        $sPhones = '';
+        foreach ($this->arrPhones as $author => $item) {
+            $sPhones .= $author . ' ; ' . implode(' ', $item['phone']) . "\n";
+        }
+
+        return file_put_contents($this->phoneFile, $sPhones);
     }
 }
--- main.php
+++ main.php
@@ -10,22 +10,26 @@ error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 
+
 $history = new TelegramHistory();
 $history->getHistory($history->inFile);
 
 $handle = @fopen($history->tmpFile, "r");
 while ($buffer = fgets($handle)) {
     $item = json_decode($buffer, true);
-    // email only
-    if (!preg_match_all('/([a-z0-9_\.\-])+\@(([a-z0-9\-])+\.)+([a-z0-9]{2,4})+/i', $item[2], $emailMatches)
-        || strpos($item[2], "FWD")
-//        || strpos($item[0], "[@MetalPlaceHelper]")
+    if (strpos($item[2], "FWD") || strpos($item[0], "[@metal_place_lotto_bot]")
+        //        || strpos($item[0], "[@MetalPlaceHelper]")
     ) {
         continue;
     }
-
-    $history->getEmails($item, $emailMatches);
+    // email only
+    if (preg_match_all('/([a-z0-9_\.\-])+\@(([a-z0-9\-])+\.)+([a-z0-9]{2,4})+/i', $item[2], $emailMatches)) {
+        $history->getEmails($item, $emailMatches);
+    }
+    // phone only
+    $history->getPhones($item);
 }
 fclose($handle);
 
 $history->storeEmails();
+$history->storePhones();
